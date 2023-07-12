c703989ee0628560e6c4287b64c14cda0c6f4ae1
KuoLinTang <linkuotang1997@gmail.com>
2023-07-12 23:48:16 +0100

    changed code to display response messages when the request succeeds. accepting status code from response. added loading spinner.

--- Groceasy/static/script.js
+++ Groceasy/static/script.js
@@ -1,21 +1,46 @@
 $(document).ready(function () {
-    $('.submit-item-name').click(function () {
-        var item_name = $('#item-name').val();
-        updateTables(item_name);
+
+    $('#submit-button').click(function (event) {
+        event.preventDefault();
+        updateTables();
     });
 
-    function updateTables(item_name) {
-        $.ajax({
-            url: '/scrap_items/',
-            method: 'POST',
-            data: { item_name: item_name },
-            success: function (response) {
-                console.log('get response');
-                displayData(response);
-            }
-        });
-    }
+    $('#item-name').keypress(function (event) {
+        if (event.which === 13) { // 按下的是Enter鍵
+            event.preventDefault(); // 阻止表單的預設提交行為
+            updateTables();
+        }
+    });
 });
+
+function updateTables() {
+    showLoadingSpinner();  // display loading spinner
+    var item_name = $('#item-name').val();  // Get item name from the input box
+
+    $.ajax({
+        url: '/scrap_items/',
+        method: 'POST',
+        data: { item_name: item_name },
+        success: function (response, textStatus, xhr) {
+
+            var status_code = xhr.status_code;
+
+            console.log('get responses');
+            hideLoadingSpinner();  // hide loading spinner
+            console.log(status_code);
+
+            if (status_code === 200) {
+                displayData(response);
+            } else {
+                throw 'Undefined status code';
+            }
+        },
+        error: function (error) {
+            hideLoadingSpinner();
+            console.log(error);
+        }
+    });
+}
 
 function displayData(data) {
     var asdaTable = $('#asda_results');
@@ -27,7 +52,6 @@ function displayData(data) {
     var sains_list = data.sains_result;
     var tesco_list = data.tesco_result;
     console.log('get result');
-    console.log(asda_list);
 
     tableInsert(asda_list, asdaTable, 'ASDA');
     tableInsert(sains_list, sainsTable, 'Sainsbury');
@@ -43,11 +67,20 @@ function tableInsert(data, table, company) {
     // 使用迴圈將數據插入表格中
     for (var i = 0; i < data.length; i++) {
         var row = $('<tr>');
-        row.append($('<td>').text(data['name']));
-        row.append($('<td>').text(data['price']));
-        row.append($('<td>').text(data['unit_price']));
-        row.append($('<td>').text(data['volume']));
+        row.append($('<td>').text(data[i]['name']));
+        row.append($('<td>').text(data[i]['price']));
+        row.append($('<td>').text(data[i]['unit_price']));
+        row.append($('<td>').text(data[i]['volume']));
         tbody.append(row);
     }
     table.append(tbody);
+}
+
+
+function showLoadingSpinner() {
+    $('#loadingSpinner').show(); // 显示加载图标
+}
+
+function hideLoadingSpinner() {
+    $('#loadingSpinner').hide(); // 隐藏加载图标
 }
