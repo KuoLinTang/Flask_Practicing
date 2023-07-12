40e9f6b716bafb2a6596465b2598050059945120
KuoLinTang <linkuotang1997@gmail.com>
2023-07-12 23:47:09 +0100

    added Http request status code in the response.

--- Groceasy/app.py
+++ Groceasy/app.py
@@ -37,35 +37,29 @@ def scrap_items():
 
     if item_name != None and len(item_name) > 0:
 
-        # n = 10
-        # grocery_store = ['asda', 'sainsbury', 'tesco']
+        n = 10
+        grocery_store = ['asda', 'sainsbury', 'tesco']
 
-        # # multiprocessing the web scraping process
-        # with ThreadPool(3) as p:
-        #     result_list = p.starmap(all_in_one.get_items, zip(
-        #         [item_name]*3, [n]*3, grocery_store))
+        # multiprocessing the web scraping process
+        with ThreadPool(3) as p:
+            result_list = p.starmap(all_in_one.get_items, zip(
+                [item_name]*3, [n]*3, grocery_store))
 
-        # asda_list = list_to_object(result_list[0], 'asda')
-        # sains_list = list_to_object(result_list[0], 'sainsbury')
-        # tesco_list = list_to_object(result_list[0], 'tesco')
+        asda_list = list_to_object(result_list[0], 'asda')
+        sains_list = list_to_object(result_list[0], 'sainsbury')
+        tesco_list = list_to_object(result_list[0], 'tesco')
 
-        # business_list = [[o.to_json() for o in asda_list], [o.to_json() for o in sains_list], [o.to_json() for o in tesco_list]]
-        asda_list = [ItemData('asda', 'milk', '1L', '2.5', '1'),
-                     ItemData('asda', 'bacon', '1g', '2.5', '1')]
-        sains_list = [ItemData('sainsbury', 'milk', '1L', '2.5', '1'),
-                      ItemData('sainsbury', 'bacon', '1g', '2.5', '1')]
-        tesco_list = [ItemData('tesco', 'milk', '1L', '2.5', '1'),
-                      ItemData('tesco', 'bacon', '1g', '2.5', '1')]
         enc_asda = [i.__dict__ for i in asda_list]
         enc_sains = [i.__dict__ for i in sains_list]
         enc_tesco = [i.__dict__ for i in tesco_list]
 
-        # response = Response(json.dumps(
-        #     {'asda_list': enc_asda, 'sains_list': enc_sains, 'tesco_list': enc_tesco}),
-        #     content_type='application/json')
+        response = jsonify(asda_result=enc_asda,
+                           sains_result=enc_sains, tesco_result=enc_tesco)
+        return response, 200
 
-        # return response
-        return jsonify(asda_result=enc_asda, sains_result=enc_sains, tesco_result=enc_tesco)
+    else:
+        response = jsonify({'message': 'invalid item name'})
+        return response, 400
 
 
 if __name__ == "__main__":
