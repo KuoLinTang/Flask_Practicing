1e59bb47b654460e84af833465015d06a9b7bfb5
KuoLinTang <linkuotang1997@gmail.com>
2023-07-29 00:51:37 +0100

    added a new function 'get_all_businesses' that can fetch items with the same item name using multi-threading pools and returns a dictionary.

--- Groceasy/grocery_scraping/all_in_one.py
+++ Groceasy/grocery_scraping/all_in_one.py
@@ -1,17 +1,31 @@
 from . import asda_func
 from . import tesco_func
 from . import sains_func
+from multiprocessing.dummy import Pool as ThreadPool
 
 
-def get_items(item: str = 'Milk', n: int = 5, business: str = 'sainsbury'):
+def get_items(item: str = 'Milk', n: int = 5, business: str = "Sainsbury's"):
 
-    if business == 'sainsbury':
+    if business == "Sainsbury's":
         item_list = sains_func.sainsbury(item=item, n=n)
-    elif business == 'asda':
+    elif business == "ASDA":
         item_list = asda_func.asda(item=item, n=n)
-    elif business == 'tesco':
+    elif business == "Tesco":
         item_list = tesco_func.tesco(item=item, n=n)
     else:
         raise Exception('Grocery store not supported.')
 
     return item_list
+
+
+def get_all_businesses(item: str = 'Milk', n: int = 5):
+
+    grocery_stores = ['ASDA', "Sainsbury's", 'Tesco']
+
+    with ThreadPool(3) as p:
+        result_list = p.starmap(get_items, zip(
+            [item]*3, [n]*3, grocery_stores))
+
+    item_list_dict = dict(zip(grocery_stores, result_list))
+
+    return item_list_dict
