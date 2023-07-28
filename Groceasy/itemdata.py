45fce3c2d2bb1b8859dbf63d7fa4c825e016c859
KuoLinTang <linkuotang1997@gmail.com>
2023-07-29 00:49:54 +0100

    moved the function 'list_to_object', which converts a list of item lists into a list of ItemData objects, from app.py to the itemdata.py

--- Groceasy/itemdata.py
+++ Groceasy/itemdata.py
@@ -1,6 +1,3 @@
-import json
-
-
 class ItemData:
 
     def __init__(self, business, name, volume, price, unit_price, img=None):
@@ -10,6 +7,22 @@ class ItemData:
         self.price = price
         self.unit_price = unit_price
         self.img = img
+
+    @staticmethod
+    def list_to_object(item_list, business):
+        result_list = []
+        for i in item_list:
+            result_list.append(
+                ItemData(
+                    business=business,
+                    name=i[0],
+                    volume=i[1],
+                    price=i[2],
+                    unit_price=i[3],
+                    img=i[4]
+                ).__dict__  # convert into JSON
+            )
+        return result_list
 
 
 if __name__ == "__main__":
